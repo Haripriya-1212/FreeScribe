bf53752d89534f03f9b2c7c6e0a9c930d64b7b2c
Haripriya <93302077+Haripriya-1212@users.noreply.github.com>
2024-08-18 10:53:50 +0530

    Update Infromation.jsx

--- frontend/src/components/Infromation.jsx
+++ frontend/src/components/Infromation.jsx
@@ -34,44 +34,44 @@ export default function Infromation(props) {
   return (
     <div className='flex flex-col max-w-[1000px]  w-full'>
 
-      <section className='min-h-screen flex flex-col'>
+        <section className='min-h-screen flex flex-col'>
 
-    <main className='flex-1 flex flex-col gap-3 sm:gap-4 justify-center p-4 text-center pb-20 w-72 max-w-prose w-full mx-auto'>
-        <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl items-center whitespace-nowrap pb-6'>Your <span className='text-blue-600 bold'>Transcrpition</span></h1>
-
-        <div className='grid-cols-2 items-center mx-auto bg-white border-1 border-solid border-blue-3 00 shadow rounded-full overflow-hidden'>
-            <button 
-            className={'px-5 py-2 font-medium duration-200' + (tab === 'transcription' ? ' bg-blue-600 text-white' : ' text-blue-400 hover:text-blue-600')}
-            onClick={() => setTab('transcription')}
-            >Transcrpition</button>            
-            {/* <button 
-            className={'px-5 py-2 font-medium duration-200' + (tab === 'translation' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}
-            onClick={() => setTab('translation')}
-            >Translation</button>             */}
-        </div>
+            <main className='flex-1 flex flex-col gap-3 sm:gap-4 justify-center p-4 text-center pb-20 w-72 max-w-prose w-full mx-auto'>
+                <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl items-center whitespace-nowrap pb-6'>Your <span className='text-blue-600 bold'>Transcrpition</span></h1>
         
-        <div className='my-8 flex flex-col'>
-
-        {/* {tab === 'transcription' ? <Transcription output={output}/> : <Translation output={output}/>} */}
-        <Transcription output={output}/>
-        </div>
-
-        <div className='flex items-center gap-4 mx-auto'>
-            <button title="Copy" 
-            className='bg-white px-2 text-blue-500 hover:text-blue-400 duration-200 rounded aspect-square grid place-items-center'
-            onClick={handleCopy}
-            >
-                <i className="fa-solid fa-copy"></i>
-            </button>
-            <button title="Download" 
-            className='bg-white px-2 text-blue-500 hover:text-blue-400 duration-200 rounded aspect-square grid place-items-center'
-            onClick={handleDownload}
-            >
-                <i className="fa-solid fa-download"></i>
-            </button>
-        </div>
-    </main>
-                </section>
+                <div className='grid-cols-2 items-center mx-auto bg-white border-1 border-solid border-blue-3 00 shadow rounded-full overflow-hidden'>
+                    <button 
+                    className={'px-5 py-2 font-medium duration-200' + (tab === 'transcription' ? ' bg-blue-600 text-white' : ' text-blue-400 hover:text-blue-600')}
+                    onClick={() => setTab('transcription')}
+                    >Transcrpition</button>            
+                    {/* <button 
+                    className={'px-5 py-2 font-medium duration-200' + (tab === 'translation' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}
+                    onClick={() => setTab('translation')}
+                    >Translation</button>             */}
                 </div>
+                
+                <div className='my-8 flex flex-col'>
+        
+                {/* {tab === 'transcription' ? <Transcription output={output}/> : <Translation output={output}/>} */}
+                <Transcription output={output}/>
+                </div>
+        
+                <div className='flex items-center gap-4 mx-auto'>
+                    <button title="Copy" 
+                    className='bg-white px-2 text-blue-500 hover:text-blue-400 duration-200 rounded aspect-square grid place-items-center'
+                    onClick={handleCopy}
+                    >
+                        <i className="fa-solid fa-copy"></i>
+                    </button>
+                    <button title="Download" 
+                    className='bg-white px-2 text-blue-500 hover:text-blue-400 duration-200 rounded aspect-square grid place-items-center'
+                    onClick={handleDownload}
+                    >
+                        <i className="fa-solid fa-download"></i>
+                    </button>
+                </div>
+            </main>
+        </section>
+    </div>
   )
 }
